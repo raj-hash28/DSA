2c5251e9f5f76d71c6e2e67881ac55459b23de06
Raj Kumar Gupta <rajkumar290503@gmail.com>
2026-07-24 23:20:06 +0530

    Time: 2 ms (78.76%), Space: 28.8 MB (19.84%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,7 +6,7 @@ public:
         stack<int> st;
         st.push(nums[n-1]);
 
-        for(int i = n-1; i >= 0; i--){
+        for(int i = n-2; i >= 0; i--){
             st.push(nums[i]);
         }
 
