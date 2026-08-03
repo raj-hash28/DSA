01d45a523cac7baadd1f2f52a75ae53dcabf9443
Raj Kumar Gupta <rajkumar290503@gmail.com>
2026-08-03 23:55:25 +0530

    Time: 3 ms (69.32%), Space: 16 MB (32.2%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(int open, int close, int n, string s) {
+        if(s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        if(open < n) {
+            solve(open + 1, close, n, s + '(');
+        }
+
+        if(close < open) {
+            solve(open, close + 1, n, s + ')');
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        solve(0, 0, n, "");
+        return ans;
+    }
+};
