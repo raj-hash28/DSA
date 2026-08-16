ebfd467cd2b4c5ac7d2edf3c4fb875d6c59f2755
Raj Kumar Gupta <rajkumar290503@gmail.com>
2026-08-16 23:10:59 +0530

    Time: 11 ms (16.75%), Space: 131.4 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for(auto i : stones){
+
+            if(i % 3 == 0) count0++;
+            else if(i % 3 == 1) count1++;
+            else count2++;
+        }
+
+        if(count0 % 2 == 0){
+            return count1 >= 1 && count2 >= 1;
+        } 
+
+        return abs(count1-count2) > 2;
+    }
+};
