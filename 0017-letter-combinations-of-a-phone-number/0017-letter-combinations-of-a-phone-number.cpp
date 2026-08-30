a5a7fcad5cab18967ebf491b7f99b83829c04f80
Raj Kumar Gupta <rajkumar290503@gmail.com>
2026-08-30 11:18:46 +0530

    Time: 3 ms (5.03%), Space: 10.3 MB (5.03%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
-    vector<string> ans;
-    vector<string> mp = {
-        "", "", "abc", "def", "ghi",
-        "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
+    void helper(string& s, int n, int idx, string& diary, vector<string>& res){
+        unordered_map<char, string> f;
 
-    void solve(int idx, string digits, string s) {
-        if(idx == digits.size()) {
-            ans.push_back(s);
+        f['2'] = "abc";
+        f['3'] = "def";
+        f['4'] = "ghi";
+        f['5'] = "jkl";
+        f['6'] = "mno";
+        f['7'] = "pqrs";
+        f['8'] = "tuv";
+        f['9'] = "wxyz";
+
+        if(idx==n){
+            res.push_back(diary);
             return;
         }
 
-        string letters = mp[digits[idx] - '0'];
+        string choices = f[s[idx]];
 
-        for(char c : letters) {
-            solve(idx + 1, digits, s + c);
+        for(int j = 0; j < choices.size(); j++){
+            diary.push_back(choices[j]);
+            helper(s, n, idx+1, diary, res);
+            diary.pop_back();
         }
+
+        return;
     }
+    vector<string> letterCombinations(string s) {
+        int n = s.size();
 
-    vector<string> letterCombinations(string digits) {
-        if(digits.empty()) {
-            return {};
-        }
-
-        solve(0, digits, "");
-        return ans;
+        if(s.empty()) return {};
+        vector<string> res;
+        string diary = "";
+        helper(s, n, 0, diary, res);
+        return res;
     }
 };
