7a465c18e14255144893120ffa0c30f2af349e94
Raj Kumar Gupta <rajkumar290503@gmail.com>
2026-09-20 23:08:38 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            char ch = s[i];
+            int alphabet = ch-'a' + 1;
+            int reverse = 27 - alphabet;
+            int position = i+1;
+
+            int product = reverse * position;
+
+            sum += product;
+        }
+
+        return sum;
+    }
+};
